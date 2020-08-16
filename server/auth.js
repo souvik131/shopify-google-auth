e398fad1cbee90d168c834e69dae4c6254370ef8
Souvik <souvik131@gmail.com>
2020-08-17 01:04:02 +0530

    fix restrictAccess code

--- server/auth.js
+++ server/auth.js
@@ -60,7 +60,7 @@ const passportAuth=(server)=>{
         })
     ))
     const restrictAccess = (ctx) => {
-        if (!ctx.isAuthenticated()) ctx.redirect("/");
+        // if (!ctx.isAuthenticated()) ctx.redirect("/");
         // next();
     };
     
