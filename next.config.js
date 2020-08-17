bcc290a676af563d68f0879c5d6c4c2de8136aa6
Souvik <souvik131@gmail.com>
2020-08-18 00:32:02 +0530

    Making structural changes

--- next.config.js
+++ next.config.js
@@ -3,10 +3,11 @@ const withCSS = require("@zeit/next-css");
 
 const webpack = require("webpack");
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
+const host = JSON.stringify(process.env.HOST);
 
 module.exports = withCSS({
   webpack: config => {
-    const env = { API_KEY: apiKey };
+    const env = { API_KEY: apiKey,HOST: host };
     config.plugins.push(new webpack.DefinePlugin(env));
     return config;
   }
