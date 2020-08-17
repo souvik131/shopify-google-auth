9ffe5b3616722d5d67d912fbe137117c3dbb0edf
Souvik <souvik131@gmail.com>
2020-08-18 02:20:51 +0530

    Added google auth payload

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,20 @@ const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 class Index extends React.Component {
   state = { open: false };
+  static async getInitialProps(ctx){
+    const cookie = ctx.req.headers.cookie;
+    const resp = await fetch(`${HOST}/init`,{
+      headers:{
+        cookie:cookie
+      },
+      method:"POST"
+    })
+    const json = await resp.json()
+    return {shop:json.shopOrigin}
+  }
   render() {
     const emptyState = !store.get('ids');
+    console.log(this.props)
     return (
       <Page
         primaryAction={{
@@ -16,7 +28,7 @@ class Index extends React.Component {
           onAction: () => this.setState({ open: true }),
         }}
       >
-        <Link url="https://theuncurbed.com/auth/google" external={true}>Login to Google</Link>
+        <Link url={`https://theuncurbed.com/auth/google?shop=${this.props.shop}`} external={true}>Login to Google for {this.props.shop}</Link>
         <ResourcePicker
           resourceType="Product"
           showVariants={false}
