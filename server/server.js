a563fb939aafb1727a98c2e2875bec9e8c4164bb
Souvik <souvik131@gmail.com>
2020-08-18 18:28:00 +0530

    Structural Changes to authentication processes and files

--- server/server.js
+++ server/server.js
@@ -7,11 +7,15 @@ import Koa from "koa";
 import next from "next";
 import Router from "koa-router";
 import { init as routerInit}  from "./router"
+import cache from "../cache/app"
 import createGoogleAuth from "./auth/createGoogleAuth"
-import afterShopifyAuth from "./auth/afterShopifyAuth"
+import  { registerWebhooks}  from './handlers/register-webhooks';
+import jwt from 'jsonwebtoken'
+const getSubscriptionUrl = require('./handlers/mutations/get-subscription-url');
+
 
 dotenv.config();
-const { SHOPIFY_API_SECRET, SHOPIFY_API_KEY, SCOPES ,LISTEN_IP,LISTEN_PORT} = process.env;
+const { SHOPIFY_API_SECRET, SHOPIFY_API_KEY, SCOPES ,HOST,JWT_SECRET,APP_NAME,LISTEN_IP,LISTEN_PORT} = process.env;
 const port = parseInt(process.env.PORT, 10) || LISTEN_PORT;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -22,17 +26,16 @@ const handle = app.getRequestHandler();
   await app.prepare();
   const server = new Koa();
   const router = new Router();
+  createGoogleAuth(server);
   server.keys = [SHOPIFY_API_SECRET];
-  
   server.use(
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET,
       scopes: [SCOPES],
-      afterAuth:afterShopifyAuth
+      afterAuth:afterAuth
     })
   );
-  createGoogleAuth(server);
 
   server.use(
     graphQLProxy({
@@ -46,3 +49,57 @@ const handle = app.getRequestHandler();
 })();
 
 
+async function afterAuth(ctx) {
+  //Auth token and shop available in session
+  //Redirect to shop upon auth
+  const { code,state } = ctx.query
+  const { shop, accessToken, _expire } = ctx.session;
+  const { name,url,email } = await getProfile(shop,accessToken);
+  cache.set(shop,{ code, state ,shop, accessToken, _expire, name, url, email })
+  const jwtAccessToken = jwt.sign({shop:shop},JWT_SECRET)
+  ctx.cookies.set("jwtAccessToken", jwtAccessToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+  });
+  ctx.cookies.set("shopOrigin", shop, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+  });
+
+  await registerWebhooks(
+    shop,
+    accessToken,
+    'PRODUCTS_CREATE'
+    `${HOST}/webhooks/products/create`,
+    ApiVersion.October19
+  ) 
+
+  ctx.redirect(`https://${shop}/admin/apps/${APP_NAME}`);
+  // await getSubscriptionUrl(ctx, accessToken, shop);
+  // ctx.redirect("/");
+}
+
+
+async function getProfile(shop,accessToken){
+    let reqUrl=`https://${shop}/admin/api/graphql.json`
+    let result = await fetch(reqUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Access-Token":accessToken
+      },
+      body: JSON.stringify({
+        query: `{
+            shop {
+              name
+              url
+              email
+            }
+          }`
+      })
+    })
+    const profile = await result.json()
+    return profile.data.shop
+}
