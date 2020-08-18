e757695d2e23989b02ea9aa70439f4c6be2b3db3
Souvik <souvik131@gmail.com>
2020-08-19 01:27:51 +0530

    Adding a loader

--- pages/index.js
+++ pages/index.js
@@ -21,16 +21,18 @@ export default function Index(statesData) {
      if(states.loading){
        return (<Spinner accessibilityLabel="Loading..." size="large" color="teal" />)
      }
-     return (
-       states.loggedIn?
-       <Page>
-        <Heading>Congrats, you are logged in! 🎉</Heading> 
-        <Link url={`https://theuncurbed.com/logout`}>Logout of google</Link>
-       </Page>:<Page>
-         <Heading>Welcome, connect with your google account 🚀</Heading> 
-         <Link url={`https://theuncurbed.com/auth/google`} external={true}>Connect to Google</Link>
-       </Page>
-     );
+     else{
+      return (
+        states.loggedIn?
+        <Page>
+          <Heading>Congrats, you are logged in! 🎉</Heading> 
+          <Link url={`https://theuncurbed.com/logout`}>Logout of google</Link>
+        </Page>:<Page>
+          <Heading>Welcome, connect with your google account 🚀</Heading> 
+          <Link url={`https://theuncurbed.com/auth/google`} external={true}>Connect to Google</Link>
+        </Page>
+      );
+     }
  }
  
  Index.getInitialProps = async ctx => {
