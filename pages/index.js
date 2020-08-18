ef956917a2d5821d85f5d970f7cdc3606428c4fd
Souvik <souvik131@gmail.com>
2020-08-19 01:19:56 +0530

    Adding a loader

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,58 @@
 import { Heading,Link, Page,Spinner } from '@shopify/polaris';
-import Router from 'next/router'
+import { useEffect, useState } from 'react';
 
-class Index extends React.Component {
+export default function Index({ statesData }) {
 
-  static async getInitialProps(ctx){
-    if(!ctx.req){
-      return {loading:true}
-    }
-
-    if(ctx.req&&ctx.req.headers&&ctx.req.headers.cookie){
-      const cookie = ctx.req.headers.cookie;
-      const resp = await fetch(`${HOST}/checkGoogleLogin`,{
-        headers:{
-          cookie:cookie
-        },
-        method:"POST"
-      })
-      const loginData = await resp.json()
-      if(loginData.loggedIn){
-        return {loggedIn:true,loading:true}
-      }
-    }
-    return {loggedIn:false,loading:true}
-    
-  }
-
-  render() {
-    if(this.props.loading){
-      return (<Spinner accessibilityLabel="Loading..." size="large" color="teal" />)
-    }
-    return (
-      this.props.loggedIn?
-      <Page>
-       <Heading>Congrats, you are logged in! 🎉</Heading> 
-       <Link url={`https://theuncurbed.com/logout`}>Logout of google</Link>
-      </Page>:<Page>
-        <Heading>Welcome, connect with your google account 🚀</Heading> 
-        <Link url={`https://theuncurbed.com/auth/google`} external={true}>Connect to Google</Link>
-      </Page>
-    );
-  }
-
-
-}
-
-export default Index;
+  const [states, setStates] = useState(statesData);
+   useEffect(() => {
+     async function loadData() {
+       const resp = await fetch(`${HOST}/checkGoogleLogin`,{
+         headers:{
+           cookie:cookie
+         },
+         method:"POST"
+       })
+       const loginData = await resp.json()
+       setStates({loggedIn:loginData.loggedIn,loading:true})
+     }
+ 
+     if(statesData.log!==false||statesData.log!==true) {
+         loadData();
+     }
+   }, {loading:true});
+ 
+     if(states.loading){
+       return (<Spinner accessibilityLabel="Loading..." size="large" color="teal" />)
+     }
+     return (
+       states.loggedIn?
+       <Page>
+        <Heading>Congrats, you are logged in! 🎉</Heading> 
+        <Link url={`https://theuncurbed.com/logout`}>Logout of google</Link>
+       </Page>:<Page>
+         <Heading>Welcome, connect with your google account 🚀</Heading> 
+         <Link url={`https://theuncurbed.com/auth/google`} external={true}>Connect to Google</Link>
+       </Page>
+     );
+ }
+ 
+ Person.getInitialProps = async ctx => {
+     if(!ctx.req) {
+         return { loading:true };
+     }
+ 
+     if(ctx.req&&ctx.req.headers&&ctx.req.headers.cookie){
+       const cookie = ctx.req.headers.cookie;
+       const resp = await fetch(`${HOST}/checkGoogleLogin`,{
+         headers:{
+           cookie:cookie
+         },
+         method:"POST"
+       })
+       const loginData = await resp.json()
+       if(loginData.loggedIn){
+         return {loggedIn:true,loading:true}
+       }
+     }
+     return {loggedIn:false,loading:true}
+ };
