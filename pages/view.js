24fb1045ef0100085a77b0820d8bda44b29fc083
Souvik <souvik131@gmail.com>
2020-08-18 23:41:22 +0530

    google login and redirection

--- pages/view.js
+++ pages/view.js
@@ -1,9 +1,36 @@
 import { Heading, Page } from "@shopify/polaris";
+import Router from 'next/router'
 
-const Main = props => {
-  return (<Page>
-            <Heading>Congrats, you are logged in! 🎉</Heading> 
-          </Page>);
+class Main extends React.Component {
+
+  static async getInitialProps(ctx){
+    if(ctx.req&&ctx.req.headers&&ctx.req.headers.cookie){
+      const cookie = ctx.req.headers.cookie;
+      const resp = await fetch(`${HOST}/checkGoogleLogin`,{
+        headers:{
+          cookie:cookie
+        },
+        method:"POST"
+      })
+      const loginData = await resp.json()
+      if(!loginData.loggedIn){
+        ctx.res.writeHead(302, {Location: '/'})
+        ctx.res.end()
+      }
+    }
+    return {}
+    
+  }
+
+  render() {
+    return (
+      <Page>
+       <Heading>Congrats, you are logged in! 🎉</Heading> 
+      </Page>
+    );
+  }
+
+
 }
 
 export default Main;
